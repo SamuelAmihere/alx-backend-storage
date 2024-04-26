50d1a11a9f9f66f41d5142315047de2ccfae8098
Samuel Amihere <amisam2000@gmail.com>
2024-04-26 08:24:47 +0800

    MySQL advanced: Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- A SQL script that creates a trigger that decreases
+-- the quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
+DROP TRIGGER IF EXISTS decrease_quantity;
+DELIMITER $$
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE item_id = NEW.item_name;
+END $$
+DELIMITER ;
