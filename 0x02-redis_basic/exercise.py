28e08308bde5888df2cde3fa021f61d417f4497c
Samuel Amihere <amisam2000@gmail.com>
2024-05-02 13:35:16 +0800

    Redis basics: Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -52,7 +52,7 @@ def replay(method: Callable) -> None:
     Displays the history of calls of a 'method'
     """
     key = method.__qualname__
-    redis = redis.Redis()
+    redis = method.__self__._redis
     count = redis.get(key)
     inputs = redis.lrange(f"{key}:inputs", 0, -1)
     outputs = redis.lrange(f"{key}:outputs", 0, -1)
